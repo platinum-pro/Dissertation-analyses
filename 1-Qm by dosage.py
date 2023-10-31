9d9783e9ff146b6f200f1eed6628bfcf21b77b1d
platinum-pro <59403503+platinum-pro@users.noreply.github.com>
2023-10-31 14:33:45 -0500

    Create 1-Qm by dosage.py

---
+++ 1-Qm by dosage.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import seaborn as sns
+import pandas as pd
+
+# Load the data
+df_wide= pd.read_csv('Study 1d.csv')
+
+# Create df
+df = df_wide.drop_duplicates(subset='ResponseId', keep='first').reset_index(drop=True)
+
+# Ensure 'alpha' values are positive before taking the logarithm
+df = df[df['alpha'] > 0]
+
+# Filter dataframe for the columns of interest
+df_interest = df[['ResponseId', 'Qo', 'Qm', 'Dosage_ann', 'Dosage_one', 'Dosage_two', 'Mode_muc', 'Mode_oral', 'Mode_sub']]
+
+# Count the data points before outlier removal
+initial_count = df_interest.shape[0]
+
+# Remove outliers for Qm
+Q1 = df_interest['Qm'].quantile(0.25)
+Q3 = df_interest['Qm'].quantile(0.75)
+IQR = Q3 - Q1
+
+filter = (df_interest['Qm'] >= Q1 - 1.5 * IQR) & (df_interest['Qm'] <= Q3 + 1.5 * IQR)
+df_interest_filtered = df_interest[filter]
+
+# Count the data points after outlier removal
+filtered_count = df_interest_filtered.shape[0]
+
+# Calculate counts for used and dropped data points
+used_data_points = filtered_count
+dropped_data_points = initial_count - filtered_count
+
+
+# Create a new column to represent the group
+# df_interest_filtered['group'] = np.where(df_interest_filtered['Dosage_ann'], 'Dosage_ann',
+#                                         np.where(df_interest_filtered['Dosage_one'], 'Dosage_one', 'Dosage_two'))
+
+# Create a new column to represent the group
+df_interest_filtered['group'] = np.where(df_interest_filtered['Mode_muc'], 'Mode_muc',
+                                         np.where(df_interest_filtered['Mode_oral'], 'Mode_oral', 'Mode_sub'))
+
+# Specify a list of colors or use a predefined palette
+colors = ['blue', 'green', 'red']
+
+# Plot box plot
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='group', y='Qm', data=df_interest_filtered, palette=colors, width=0.5)
+
+title_str = (f"Average for each Mode Group\n"
+             f"Used Data Points: {used_data_points} | Dropped Data Points: {dropped_data_points}")
+
+plt.title(title_str)
+plt.ylabel('Qm')
+plt.xlabel('Mode of Administration Group')
+plt.show()
